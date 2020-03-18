7e2a99bedb5c1536a4be02a94b9d5259e15340d3
Mark Nikerin <mrnikerin@gmail.com>
2020-03-18 21:00:14 +0400

    TnH-3 Divide app.py file into the three parts

---
+++ TnH_app/views.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+from . import app
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
