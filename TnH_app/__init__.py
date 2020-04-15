889c5885de19bf329e16174ebfbc6c8a58622fa3
Narek Gevorkyan <mrnarek149@gmail.com>
2020-04-16 00:32:43 +0400

    Create Options Table

--- TnH_app/__init__.py
+++ TnH_app/__init__.py
@@ -6,3 +6,19 @@ app = Flask(__name__, instance_relative_config=False)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/Toast-N-Honey.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+class Options(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    productId = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    weight = db.Column(db.Integer)
+    amount = db.Column(db.Integer)
+
+    def __init__(self, ProductId, Price, Weight, Amount):
+        self.ProductId = ProductId
+        self.Price = Price
+        self.Weight = Weight
+        self.Amount = Amount
+
+    def __repr__(self):
+        return '<Option %d: ProductID: %d, Price: %d, Weight: %d, Amount: %d>' % (self.OptionId, self.ProductId, self.Price, self.Weight, self.Amount)
