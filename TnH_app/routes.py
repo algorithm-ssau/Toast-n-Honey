2bb69dac6cbd81cd3632fbcb821f0097e94743a1
mark-nikerin <mark.nikerin@yandex.ru>
2020-04-15 23:16:13 +0400

    Format code

--- TnH_app/routes.py
+++ TnH_app/routes.py
@@ -3,25 +3,31 @@ from flask_sqlalchemy import SQLAlchemy
 
 from . import app
 
+
 @app.route("/")
 def home():
     return render_template("home.html")
+
 
 @app.route("/contacts/")
 def contacts():
     return render_template("contacts.html")
 
+
 @app.route("/cheesecakes/")
 def cheesecakes():
     return render_template("cheesecakes.html")
+
 
 @app.route("/cakes/")
 def cakes():
     return render_template("cakes.html")
 
+
 @app.route("/macarons/")
 def macarons():
     return render_template("macarons.html")
+
 
 @app.route("/desserts/")
 def desserts():
