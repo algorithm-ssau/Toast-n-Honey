ae00a82312b3b522522101a08cee8124cbdbcb98
mark-nikerin <mark.nikerin@yandex.ru>
2020-04-15 23:15:59 +0400

    Update database config

--- TnH_app/__init__.py
+++ TnH_app/__init__.py
@@ -3,6 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__, instance_relative_config=False)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/Toast-N-Honey.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
