936d57bca5442b3872411cca152c8fd64ef32d0b
Eleonora Radina <61162413+eleonora-radina@users.noreply.github.com>
2020-04-18 01:35:21 +0300

    TnH-16 Добавила в assets нужные файлы

--- TnH_app/assets.py
+++ TnH_app/assets.py
@@ -1,3 +1,28 @@
 from flask_assets import Bundle
 
 
+def compile_assets(assets):
+
+    js = Bundle('vendor/jquery/jquery-3.2.1.min.js',
+                'vendor/animsition/js/animsition.min.js',
+                'vendor/bootstrap/js/popper.js',
+                'vendor/bootstrap/js/bootstrap.min.js',
+                'js/main.js',
+                filters='jsmin',
+                output='js/all-min.js')
+
+    css = Bundle('vendor/bootstrap/css/bootstrap.min.css',
+                 'fonts/font-awesome-4.7.0/css/font-awesome.min.css',
+                 'fonts/themify/themify-icons.css',
+                 'fonts/Linearicons-Free-v1.0.0/icon-font.min.css',
+                 'fonts/elegant-font/html-css/style.css',
+                 'vendor/animate/animate.css',
+                 'vendor/animsition/css/animsition.min.css',
+                 'vendor/noui/nouislider.min.css',
+                 'css/util.css',
+                 'css/main.css',
+                 filters='cssmin',
+                 output='css/all-min.css')
+
+    assets.register('js_all', js)
+    assets.register('css_all', css)
