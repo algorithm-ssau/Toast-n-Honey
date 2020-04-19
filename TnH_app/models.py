8790510c944b810d3070641243c4b4a17cc4f30a
Valery Koshkin <valkoshkin99@gmail.com>
2020-04-19 18:04:42 +0400

    Описал сущность Orders

--- TnH_app/models.py
+++ TnH_app/models.py
@@ -1,1 +1,15 @@
 from . import db
+
+class Orders(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    customerId = db.Column(db.Integer, db.ForeignKey('customers.id'), nullable=False)
+    productId = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+    optionId = db.Column(db.Integer, db.ForeignKey('options.id'), nullable=False)
+
+    def __init__(self, CustomerId, ProductId, OptionId):
+        self.CustomerId = CustomerId
+        self.ProductId = ProductId
+        self.OptionId = OptionId
+
+    def __repr__(self):
+        return '<Order %d: CustomerID: %d, ProductID: %d, OptionID: %d>' % (self.OrderId, self.CustomerId, self.ProductId, self.OptionId)
