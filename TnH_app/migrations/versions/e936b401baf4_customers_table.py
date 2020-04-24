1a2de9835247719b7463a988424d5d033dad6849
mark-nikerin <mark.nikerin@yandex.ru>
2020-04-25 00:59:43 +0400

    Добавил порядок миграций

--- TnH_app/migrations/versions/e936b401baf4_customers_table.py
+++ TnH_app/migrations/versions/e936b401baf4_customers_table.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'e936b401baf4'
-down_revision = None
+down_revision = '315f0946de5b'
 branch_labels = None
 depends_on = None
 
