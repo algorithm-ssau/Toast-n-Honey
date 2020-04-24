e26610b22ff9ce58d29070820555c2bc6d6f8473
mark-nikerin <mark.nikerin@yandex.ru>
2020-04-25 00:19:26 +0400

    Cherry Pick

--- TnH_app/models.py
+++ TnH_app/models.py
@@ -31,3 +31,17 @@ class Options(db.Model):
 
     def __repr__(self):
         return '<Option %d: ProductID: %d, Price: %d, Weight: %d, Amount: %d>' % (self.id, self.productId, self.price, self.weight, self.amount)
+
+class Customers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.NVARCHAR(100), nullable=False)
+    Phone = db.Column(db.NVARCHAR(10), nullable=False)
+    Email= db.Column(db.NVARCHAR(100))
+
+    def __init__(self, Name, Phone, Email):
+        self.Name = Name
+        self.Phone = Phone
+        self.Email = Email
+
+    def __repr__(self):
+        return '<Customer %d: Name: %c, Phone: %c, Email: %c>' % (self.id, self.Name, self.Phone, self.Email)
